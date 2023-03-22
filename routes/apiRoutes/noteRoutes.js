11c9bf883f1934f56d61956a36fe04d0acd33b5b
victorlmorales <victor.l.morales2020@gmail.com>
2023-03-22 00:02:42 -0500

    Added apiRoutes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const {notes} = require('../../db/db.json');
+const {createNewNote, deleteNote} = require('../../lib/notes');
+
+
+router.get('/notes', (req, res) => {
+    let saved = notes;
+    res.json(saved);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = createNewNote(req.body, notes);
+    res.json(note);
+})
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+})
+
+
+module.exports = router;
